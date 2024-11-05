279d0bebc1cddc967a6de9ddea868fa503d0637b
kellianb <kellian.bechtel@hotmail.com>
2024-11-05 09:07:38 +0100

    Updated DB - Added sessions table, to store user sessions - Formatted SQL code - Added sample data with hashed passwords to user table

--- sql/03-tables.sql
+++ sql/03-tables.sql
@@ -3,20 +3,20 @@
 CREATE TABLE "messages"
 (
     "id"        serial PRIMARY KEY,
-    "sent_at"   timestamp not null,
+    "sent_at"   timestamp    not null,
     "content"   varchar(255) not null,
-    "sender_id" int not null,
-    "deleted"   bool not null default FALSE
+    "sender_id" int          not null,
+    "deleted"   bool         not null default FALSE
 );
 
 CREATE TABLE "users"
 (
-    "id"         serial PRIMARY KEY,
-    "username"   varchar(255) not null,
+    "id"            serial PRIMARY KEY,
+    "username"      varchar(255) not null UNIQUE,
     "password_hash" varchar(128) not null,
-    "role_id"    int not null,
-    "created_at" timestamp not null,
-    "deleted"   bool not null default FALSE
+    "role_id"       int          not null,
+    "created_at"    timestamp    not null,
+    "deleted"       bool         not null default FALSE
 );
 
 CREATE TABLE "roles"
@@ -25,11 +25,22 @@ CREATE TABLE "roles"
     "name" varchar(255) not null
 );
 
+CREATE TABLE "sessions"
+(
+    "token"      varchar(48) not null,
+    "user_id"    int         not null,
+    "created_at" timestamp   not null,
+    "expires_at" timestamp   not null
+);
+
 ALTER TABLE "users"
     ADD FOREIGN KEY ("role_id") REFERENCES "roles" ("id");
 
 ALTER TABLE "messages"
     ADD FOREIGN KEY ("sender_id") REFERENCES "users" ("id");
+
+ALTER TABLE "sessions"
+    ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
 alter table messages
     owner to kellian;
@@ -38,4 +49,7 @@ alter table users
     owner to kellian;
 
 alter table roles
+    owner to kellian;
+
+alter table sessions
     owner to kellian;
--- sql/04-data.sql
+++ sql/04-data.sql
@@ -5,18 +5,9 @@ VALUES ('admin'),
        ('moderator'),
        ('user');
 
-
-/*
-INSERT INTO users (username, role_id, created_at) VALUES
-
-('alice', 1, '2024-01-01 10:00:00'),
-('bob', 3, '2024-01-02 11:00:00'),
-('charlie', 3, '2024-01-03 12:00:00'),
-('dave', 2, '2024-01-04 13:00:00'),
-('eve', 2, '2024-01-05 14:00:00'),
-('frank', 1, '2024-01-06 15:00:00'),
-('grace', 2, '2024-01-07 16:00:00'),
-('heidi', 3, '2024-01-08 17:00:00'),
-('ivan', 1, '2024-01-09 18:00:00'),
-('judy', 3, '2024-01-10 19:00:00');
-*/
+-- The password for all test users is 123321
+INSERT INTO users (username, password_hash, role_id, created_at) VALUES
+('John', '$argon2id$v=19$m=19456,t=2,p=1$HdaU+syjZcOlv8tzNJ4LMQ$xzLhx6dObk4p1565R9A+lO9cAi9pT6WKDbDFDyONNH8', 1, '2024-10-15 07:36:55.453511'),
+('Joe', '$argon2id$v=19$m=19456,t=2,p=1$J8tKDQlTRSxcQKsSzPD1LQ$nRgnXxU9ckY5We3XCR/auJVSZ4RWduqol4b3gFuTd4c', 1, '2024-10-15 07:37:15.661956'),
+('Luke', '$argon2id$v=19$m=19456,t=2,p=1$ZjFLuhsuk3iY+pjw43y2PA$euXotJAzL+3oIgUhKM2PT5w64pL23sfD4pQ+o7PMdi0', 1, '2024-10-15 07:37:48.945183'),
+('Alice', '$argon2id$v=19$m=19456,t=2,p=1$sm7ecGQHufz0HMCZQ/UibA$2j8/FoApU1ChLRXJgdffwrGVCREEjAKJ8phV2sgROsM', 1, '2024-10-15 07:37:52.197724');
